def2d89c602e7802d3c26ff836d9403e1b723f55
Kai Kratz <Kai.Kratz@ecmwf.int>
2025-02-27 14:47:32 +0100

    wip

--- demo/demo.py
+++ demo/demo.py
@@ -408,7 +408,8 @@ def simulate_training_cmd(args):
     dates = data.shape[0]
     base_date_access_order = list(range(0, dates - 2))
 
-    for idx in tqdm.tqdm(base_date_access_order):
+    for idx in tqdm.tqdm(base_date_access_order, disable=not args.progress):
+        logger.info("Processing chunks[{idx}, {idx+1}, {idx+2}]")
         np.mean(data[idx], axis=2).squeeze()
         np.mean(data[idx + 1], axis=2).squeeze()
         np.mean(data[idx + 2], axis=2).squeeze()
